95ff7f6ea0a59fe1bc227dcd4b8f62aad2f42a44
Thomas Jez <thomas.jez@gmx.de>
2015-04-11 20:13:27 +0200

    vCard reader added

--- src/SimpleCSV/SimpleCSV.php
+++ src/SimpleCSV/SimpleCSV.php
@@ -37,4 +37,48 @@ class SimpleCSV {
         fclose($csvFile);
         return $csvArray;
     }
+
+    public function vCard2Csv($vcfName, $csvName)
+    {
+        $cardFile = fopen($vcfName, 'r');
+        $vCardTemplate = array();
+        while (($data = fgets($cardFile)) !== FALSE) {
+            $data = rtrim($data);
+            if (in_array($data, array('BEGIN:VCARD', 'END:VCARD'))) {
+                continue;
+            }
+            $dataArray = explode(':', $data);
+            if ($dataArray[0] === 'VERSION') {
+                continue;
+            }
+            $vCardTemplate[str_replace(';', ' ', $dataArray[0])] = '';
+        }
+//var_dump(($cardKeys));
+        fclose($cardFile);
+
+        $csvFile = fopen($csvName, 'w');
+        fputcsv($csvFile, array_keys($vCardTemplate));
+        $cardFile = fopen($vcfName, 'r');
+        $cardList = array();
+        while (($data = fgets($cardFile)) !== FALSE) {
+            $data = rtrim($data);
+            if ($data === 'BEGIN:VCARD') {
+                $vCard = $vCardTemplate;
+                continue;
+            }
+            if ($data === 'END:VCARD') {
+                $cardList[] = $vCard;
+                fputcsv($csvFile, $vCard);
+                continue;
+            }
+            $dataArray = explode(':', $data);
+            if ($dataArray[0] === 'VERSION') {
+                continue;
+            }
+            $vCard[str_replace(';', ' ', $dataArray[0])] = str_replace(';', ' ', $dataArray[1]);
+        }
+        var_dump(($cardList));
+        fclose($cardFile);
+        fclose($csvFile);
+    }
 }
